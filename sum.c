fa66cc3544219270c590d53986887980346b4d94
Mohamed Amoussaoui <mamoussa@e2r8p2.1337.ma>
2020-11-24 19:23:59 +0100

    sum

--- sum.c
+++ sum.c
@@ -2,11 +2,11 @@
 
 int main(void)
 {
-	int a,b;
-	printf("donnez a:\n");
-	scanf("%d", &a);
-	printf("donnez b:\n");
-	scanf("%d", &b);
-	printf("la somme est: %d\n", a+b);
+	int a,b; //initialisation des variables
+	printf("donnez a:\n"); //Afficher un message a l'utilisateur pour entrer a
+	scanf("%d", &a); // enregistrer le nombre a
+	printf("donnez b:\n"); //afficher un message a l'utilisateur pour entrer b
+	scanf("%d", &b);// enregistrer le nombre b
+	printf("la somme est: %d\n", a+b); // faire l'equation a+b et afficher la somme
 	return 0;
 }
