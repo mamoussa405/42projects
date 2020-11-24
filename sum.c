d081a836c4e0c0b419f3c745bd3335be5533d13d
Mohamed Amoussaoui <mamoussa@e2r8p2.1337.ma>
2020-11-24 19:17:48 +0100

    sum

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a,b;
+	printf("donnez a:\n");
+	scanf("%d", &a);
+	printf("donnez b:\n");
+	scanf("%d", &b);
+	printf("la somme est: %d\n", a+b);
+	return 0;
+}
