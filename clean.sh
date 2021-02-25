cb97e78b429c9db05ead0a1bd849006492057c24
Mohamed Amoussaoui <mamoussa@e2r8p4.1337.ma>
2021-02-25 10:57:00 +0100

    cashe cleaner

---
+++ clean.sh
@@ -0,0 +1,6 @@
+rm -rf ~/Library/Caches/* 
+rm -rf ~/Library/Application\ Support/Slack/Cache/*
+rm -rf ~/Library/Application\ Support/Slack/Service Worker/CacheStorage/*
+rm -rf ~/Library/Application\ Support/Code/Cache/*
+rm -rf ~/Library/Application\ Support/Code/CachedData/*
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/*
